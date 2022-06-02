62a23756b4cf4b67cc1e13047184a5be55ad47e7
MayconL27 <mayconleandro2008@gmail.com>
2022-06-02 20:09:49 -0300

    ❌ Incompleto

---
+++ Desafio03.js
@@ -0,0 +1,24 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+const c = parseInt(gets());
+var v = new Array(c);
+
+for (let i = 0; i < c; i++){
+  v[i] = parseInt(gets());
+}
+
+let  primeiroInscrito = true;
+
+const teste  = Number(v.shift());
+
+
+for(              ) {
+  const bobo  = Number(v[i]);
+
+  if (              ) {
+    primeiroInscrito = false;
+  }
+};
+
+print(primeiroInscrito ? "S" : "N");
